29be4423a17a6f4a8b8bbed5a7c5c93cb8cf8ec6
LookasWasTaken~ <piacen.luca@gmail.com>
2023-03-22 15:24:51 +0100

    add team members array

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,42 @@ E ricordiamoci che console.log() è nostro amico! */
 // Scott Estrada	Developer	scott-estrada-developer.jpg
 // Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
 
+// Genero un array contenente le proprietà di ogni singolo membro del team
+
+const teamMembers = [
+  {
+    name: "Wayne Barnett",
+    role: "Founder & CEO",
+    photo: "./assets/img/wayne-barnett-founder-ceo.jpg",
+  },
+
+  {
+    name: "Angela Caroll",
+    role: "Chief Editor",
+    photo: "./assets/img/angela-caroll-chief-editor.jpg",
+  },
+
+  {
+    name: "Walter Gordon",
+    role: "Office Manager",
+    photo: "./assets/img/walter-gordon-office-manager.jpg",
+  },
+
+  {
+    name: "Angela Lopez",
+    role: "Social Media Manager",
+    photo: "./assets/img/angela-lopez-social-media-manager.jpg",
+  },
+
+  {
+    name: "Scott Estrada",
+    role: "Developer",
+    photo: "./assets/img/scott-estrada-developer.jpg",
+  },
+
+  {
+    name: "Barbara Ramos",
+    role: "Graphic Designer",
+    photo: "./assets/img/barbara-ramos-graphic-designer.jpg",
+  },
+];
